d2b92219c2ead762af8f46193bc061b668db3f0f
Sean Park <sunjun1225@gmail.com>
2021-11-29 01:42:06 +0900

    Done up to test data

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,13 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+
+def test_row_count(data: pd.DataFrame):
+    assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data: pd.DataFrame, min_price, max_price):
+    """
+    Apply a threshold on the KL divergence to detect if the distribution of the new data is
+    significantly different than that of the reference dataset
+    """
+    assert data['price'].between(min_price, max_price).all()
